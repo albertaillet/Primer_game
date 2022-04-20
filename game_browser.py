a0d8d7ca59d6810fbf04b4a0f28ed20ea4c6b0bf
Albert Sund Aillet <albert@sundaillet.com>
2022-04-20 23:25:26 +0200

    changed data update logic

--- game_browser.py
+++ game_browser.py
@@ -23,21 +23,29 @@ geckodriver_path = r"C:\Users\alber\Documents\My_Code\Primer_game\geckodriver.ex
 
 class CoinGameBrowser(CoinGame):
 
-    def __init__(self, 
+    def __init__(self,
                  driver=None,
                  window_size=(465, 820),
-                 label_animation_time=1.0,
-                 game_over_animate_time=3.0,
+                 initial_flip_animation_time=2.0,
+                 label_animation_time=2.0,
+                 done_animation_time=3.0
                  ):
-        
+        # initial_flip_animation_time has to be at least 1.0
+        # label_animation_time has to be at least 1.0
+        # doen_animation_time has to be at least label_animation_time
+        assert initial_flip_animation_time >= 1.0
+        assert label_animation_time >= 1.0
+        assert done_animation_time >= label_animation_time
+
         driver_not_provided = (driver is None)
         if driver_not_provided:
             driver = CoinGameBrowser.get_driver()
 
         self.driver = driver
         self.window_size = window_size
+        self.initial_flip_animation_time = initial_flip_animation_time
         self.label_animation_time = label_animation_time
-        self.done_animation_time = game_over_animate_time
+        self.done_animation_time = done_animation_time
         self.reset_window()
 
         super().__init__()
@@ -59,7 +67,6 @@ class CoinGameBrowser(CoinGame):
             time.sleep(2)  # wait for starting animation to finish
 
         self.initial_flip = True
-        self.last_action = None
         self.crop_heads, self.crop_tails, self.crop_score = self.get_page_crops()
         self.toggle_show_flipping_animations()
     
@@ -70,13 +77,12 @@ class CoinGameBrowser(CoinGame):
         self.flips_left = self.start_flips
         self.done = False
         self.initial_flip = True
-        self.last_action = None
         self.crop_heads, self.crop_tails, self.crop_score = self.get_page_crops()
 
     def _wait_for_loading(self):
         iframe = self.driver.find_elements_by_tag_name('iframe')[0]
         self.driver.switch_to.frame(iframe)
-        WebDriverWait(self.driver, 200).until(
+        WebDriverWait(self.driver, 600).until(
                   lambda d: d.find_element_by_id("unity-progress-bar-full")
                              .get_attribute("style") == "width: 100%;")
         self.driver.switch_to.default_content()
@@ -88,7 +94,7 @@ class CoinGameBrowser(CoinGame):
         driver.get(game_website_link)
 
         # Set correct zoom level.
-        driver.set_context("chrome") 
+        driver.set_context("chrome")
         win = driver.find_element_by_tag_name("body")
         for _ in range(4):
             win.send_keys(Keys.CONTROL, "-")
@@ -122,58 +128,56 @@ class CoinGameBrowser(CoinGame):
     def observe(self) -> tuple:  
         return self.heads, self.tails, self.flips_left
 
-    def _update_data(self):
-        crop_heads, crop_tails, crop_score = self.get_page_crops()
-
-        if self.last_action == "flip":
-            heads_changed = crop_heads != self.crop_heads
-            tails_changed = crop_tails != self.crop_tails
-            if heads_changed and not tails_changed:
-                self.heads += 1
-            elif tails_changed and not heads_changed:
-                self.tails += 1
-            elif heads_changed and tails_changed and self.initial_flip:
-                # hack to find if the initial flip was heads or tails.
-                # if heads = 0 and tails = 1 sum is 79,055 and 170,932
-                # if heads = 1 and tails = 0 sum is 99,266 and 153,675 
-                mean_heads = np.asarray(crop_heads).mean()
-                means_tails = np.asarray(crop_tails).mean()
-                if 220 < mean_heads and means_tails < 215:
-                    self.heads += 1
-                elif 220 < means_tails and mean_heads < 215:
-                    self.tails += 1
-                else:
-                    raise ValueError(f"Unknown flip, {mean_heads}, {means_tails}")
-        elif self.last_action == "label": 
-            score_changed = crop_score != self.crop_score
-            if score_changed:
-                self.score += 1
-                self.flips_left += self.correct_label_bonus
-            else:
-                self.flips_left += self.incorrect_label_penalty
-                if self.flips_left < 0:
-                    self.done = True
-        
-        self.crop_score = crop_score
-        self.crop_heads = crop_heads
-        self.crop_tails = crop_tails
-
-    def _click_location(self, x, y):
+    def _click_location(self, x, y, label=False):
         (self.action_chain.move_to_element_with_offset(self.element, x, y)
                           .click()
                           .perform())
         self.action_chain.reset_actions()
         for device in self.action_chain.w3c_actions.devices:
             device.clear_actions()
-        if (self.heads == 0 and self.tails == 0) or self.last_action == "label":
-            time.sleep(self.label_animation_time)
 
     def flip_one_coin(self):
-        self.last_action = "flip"
         if (not self.done) and (self.flips_left > 0):
             self._click_location(*self.clicking_locations["flip_one"])
-            self.flips_left -= 1
-        self._update_data()
+            tries = 0
+            while tries < 3:
+                tries += 1
+                if self.initial_flip:
+                    time.sleep(self.initial_flip_animation_time)
+                crop_heads, crop_tails, _ = self.get_page_crops()
+                heads_changed = crop_heads != self.crop_heads
+                tails_changed = crop_tails != self.crop_tails
+                if heads_changed and not tails_changed and not self.initial_flip:
+                    self.heads += 1
+                    self.flips_left -= 1
+                    break
+                elif tails_changed and not heads_changed and not self.initial_flip:
+                    self.tails += 1
+                    self.flips_left -= 1
+                    break
+                elif heads_changed and tails_changed and self.initial_flip:
+                    # hack to find if the initial flip was heads or tails.
+                    # if heads = 0 and tails = 1 sum is 79,055 and 170,932
+                    # if heads = 1 and tails = 0 sum is 99,266 and 153,675 
+                    mean_heads = np.asarray(crop_heads).mean()
+                    means_tails = np.asarray(crop_tails).mean()
+                    if 220 < mean_heads and means_tails < 215:
+                        self.heads += 1
+                        self.flips_left -= 1
+                        self.initial_flip = False
+                    elif 220 < means_tails and mean_heads < 215:
+                        self.tails += 1
+                        self.flips_left -= 1
+                        self.initial_flip = False
+                    else:
+                        raise ValueError(f"Unknown new flip {mean_heads}, {means_tails}, {self.initial_flip}")
+                    break
+            if tries == 3:
+                raise ValueError(f"Unknown flip {heads_changed}, {tails_changed}, {self.initial_flip}")
+
+            self.crop_heads = crop_heads
+            self.crop_tails = crop_tails
+            
     
     def flip_five_coins(self):
         for _ in range(5):
@@ -190,23 +194,32 @@ class CoinGameBrowser(CoinGame):
         self._label("label_cheater")
 
     def _label(self, label):
-        self.last_action = "label"
         if not self.done:
-            self._click_location(*self.clicking_locations[label])
+            self._click_location(*self.clicking_locations[label], label=True)
             if self.flips_left+self.incorrect_label_penalty <= 0:
                 time.sleep(self.done_animation_time)
             else:
                 time.sleep(self.label_animation_time)
-            self._update_data()
+            self.crop_heads, self.crop_tails, crop_score = self.get_page_crops()
+            score_changed = crop_score != self.crop_score
+            if score_changed:
+                self.score += 1
+                self.flips_left += self.correct_label_bonus
+            else:
+                self.flips_left += self.incorrect_label_penalty
+                if self.flips_left < 0:
+                    self.done = True
             self.heads = 0
             self.tails = 0
+            self.crop_score = crop_score
+            self.initial_flip = True
 
     def reset_game(self):
         if self.done:
+            time.sleep(self.done_animation_time)
             self._click_location(*self.clicking_locations["reset"])
             self.reset_window()
             time.sleep(self.label_animation_time)
-            self._update_data()
             self.reset_values()
     
     def restart_browser(self):
